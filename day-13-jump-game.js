967e1e19a80cecdbd7f833d9b71ecbeac9973b95
Caleb Belkin <calebbelkin@gmail.com>
2024-06-14 08:29:37 -0700

    day 13, still needs work

---
+++ day-13-jump-game.js
@@ -0,0 +1,22 @@
+// You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+// Return true if you can reach the last index, or false otherwise.
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    if (nums.length == 1) return true
+    if (nums.includes(0)) return false
+for (let i = 1; i < nums.length; i++ ) {
+let len = nums.length
+if (len - i <= (nums[i] + 1)) {
+    return true
+}
+
+else return false
+}
+};
+
+
