733cdde4bc3ce8f60f493e9d9590b3dfe067be03
Caleb Belkin <calebbelkin@gmail.com>
2024-06-14 09:13:58 -0700

    day 13

--- day-13-jump-game.js
+++ day-13-jump-game.js
@@ -6,17 +6,28 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var canJump = function(nums) {
+//     if (nums.length == 1) return true
+//     if (nums.includes(0)) return false
+// for (let i = 1; i < nums.length; i++ ) {
+// let len = nums.length
+// if (len - i <= (nums[i] + 1)) {
+//     return true
+// }
+
+// else return false
+// }
+// };
+
 var canJump = function(nums) {
-    if (nums.length == 1) return true
-    if (nums.includes(0)) return false
-for (let i = 1; i < nums.length; i++ ) {
-let len = nums.length
-if (len - i <= (nums[i] + 1)) {
-    return true
-}
+    let goal = nums.length - 1;
 
-else return false
-}
-};
+     for (let i = nums.length - 2; i >= 0; i--) {
+        if (i + nums[i] >= goal) {
+            goal = i
+        }
+     }
+        return goal === 0 
+     }
 
-
+     // simpler solution 
